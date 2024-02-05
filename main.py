8f6c81d7466314b72b868180759791c27bd4435e
BenderScript <6779302+BenderScript@users.noreply.github.com>
2024-02-05 10:33:30 -0800

    Updating catch exceptions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import openai
 from dotenv import load_dotenv
-from openai import AzureOpenAI
+from openai import AzureOpenAI, AuthenticationError
 
 load_dotenv()
 
@@ -28,13 +28,19 @@ try:
         ],
     )
     print(completion.choices[0].message.content)
+except AuthenticationError as e:
+    print(e)
+    print(e.status_code)
 except openai.APIConnectionError as e:
     print("The server could not be reached")
     print(e.__cause__)  # an underlying Exception, likely raised within httpx.
 except openai.RateLimitError as e:
     print("A 429 status code was received; we should back off a bit.")
+    print(e.status_code)
 except openai.APIStatusError as e:
     print("Another non-200-range status code was received")
     print(e.status_code)
     print(e.response)
+except Exception as e:
+    print(e)
 
